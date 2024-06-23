1f1696a33fb0eac6dfb3b5d103377c817e9a60e1
Ashutosh Sonel <ashutoshhoonmain@gmail.com>
2024-06-23 17:45:35 +0530

    Updated the backend url

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 
 // we need to pass the baseURL as an object
 const API = axios.create({
-  baseURL: 'http://localhost:5500/',
+  // baseURL: 'http://localhost:5500/',
+  baseURL: 'https://sai-seva-samiti-backend.vercel.app',
 })
 
 export default API
